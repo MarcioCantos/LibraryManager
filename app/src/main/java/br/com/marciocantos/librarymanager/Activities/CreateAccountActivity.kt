d555e6d179ea4b890548c51bc2bedb35cd920755
Cantos-Notebook\Marcio Cantos <cantoscorrea@gmail.com>
2018-03-10 21:48:52 -0300

    Starter
    Começo do projeto

---
+++ app/src/main/java/br/com/marciocantos/librarymanager/Activities/CreateAccountActivity.kt
@@ -0,0 +1,13 @@
+package br.com.marciocantos.librarymanager.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.marciocantos.librarymanager.R
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
--- app/src/main/java/br/com/marciocantos/librarymanager/Activities/LoginActivity.kt
+++ app/src/main/java/br/com/marciocantos/librarymanager/Activities/LoginActivity.kt
@@ -1,18 +1,19 @@
-package br.com.marciocantos.librarymanager
+package br.com.marciocantos.librarymanager.Activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.marciocantos.librarymanager.R
 import com.google.firebase.auth.FirebaseAuth
 
 
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         mAuth = FirebaseAuth.getInstance();
     }
